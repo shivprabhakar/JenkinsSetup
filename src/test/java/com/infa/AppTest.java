c4615366106af08fa04dafa489ca42e35a655c3e
Prabhakar <shiprabhakar@informatica.com>
2020-01-25 19:10:02 +0000

    added hello jenkins

---
+++ src/test/java/com/infa/AppTest.java
@@ -0,0 +1,21 @@
+package com.infa;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        System.out.println("Hello Jenkins!!!");
+        assertTrue( true );
+    }
+}
